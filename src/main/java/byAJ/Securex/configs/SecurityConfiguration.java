2a42fcd05f681b15c872f091fbef593c0621a6d0
Justin Gross <jmbg144@gmail.com>
2017-06-28 14:29:28 -0400

    completed through step 4, added other users, insecure pages, and role based security

--- src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
+++ src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
@@ -19,16 +19,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/","/books/list","/css/**").permitAll()
+                    .antMatchers("/books/edit/**","/books/delete/**").hasRole("ADMIN")
                     .anyRequest()
                     .authenticated()
-                    .and()
-                .formLogin()
+                .and()
+                    .formLogin()
                     .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .logout()
                     .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+             auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("dave").password("begreat").roles("USER").and()
+                .withUser("fi").password("becold").roles("USER").and()
+                .withUser("root").password("justinisawesome").roles("ADMIN");
+
+
     }
 }
